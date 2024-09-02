3454ef8e28da168ab8516c849ff44a731b828c17
Azamat Jumamuratov <azamatjumamuratov2004@gmail.com>
2024-09-02 17:50:15 +0500

    General commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,17 @@ import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
+  BrowserRouter,
 } from "react-router-dom";
 import ErrorPage404 from "./Components/ErrorPage404/ErrorPage404.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} errorElement={<ErrorPage404 />} />
-  )
+  ),
+  {
+    basename: import.meta.env.BASE_URL,
+  }
 );
 
 createRoot(document.getElementById("root")).render(
